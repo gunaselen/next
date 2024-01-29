824c130686e2f8d159003c1bd1ae9d2e67382a72
Govindhasamy, Gunasekaran <gunaselen1995@gmail.com>
2024-01-29 11:26:57 +0530

    xml update

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,16 +1,37 @@
 'use client';
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Header.scss"
 import { Logo, ProfileIMg, menuIcon } from '@/utils/images'
 import Image from 'next/image'
+import { useSession, signIn, signOut } from "next-auth/react";
 
 
 const onClick = () => {
     location.href = `/api/auth/signin`
-  }
+}
 
 export default function Header() {
+
+    const { data: session, status } = useSession();
+
+    useEffect(() => {
+        console.log(session,status);
+        if (status === "authenticated" && session) {
+            if (session) {
+                
+            }
+        }
+    }, [session, status]);
+
+    const getUserName = () => {
+        return session && (
+            <span>
+                <Image src={ProfileIMg} alt="" priority />
+                {session?.user?.name}
+            </span>
+        );
+    };
 
 
     return (
@@ -19,14 +40,19 @@ export default function Header() {
                 <Image src={Logo} alt="" />
 
             </div>
-            
+
             <div className="profile">
-                <div className="profile-img" onClick={onClick}>
-                    <Image src={ProfileIMg} alt="" priority />
-                </div>
-                <div className="profile-menu">
-                    <Image src={menuIcon} alt="" priority />
-                </div>
+                {!session ? (
+                    <a onClick={() => { signIn('azure-ad') }}>
+                        Sign In
+                    </a>
+                ) : (
+                    <div className="profile-img" onClick={() => { signOut()}} >
+                        {getUserName()}
+                    </div>)}
+                    <div className="profile-menu">
+                        <Image src={menuIcon} alt="" priority />
+                    </div>
 
             </div>
         </header>
